c82ca2483856d62f4ef9f639078da330f8ad1306
Victor de Paula <victorssspaula@live.com>
2021-07-17 21:48:58 -0300

    Exercicio 19

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const checkBox = document.getElementById('agreement');
+const buttonEnviar = document.getElementById('submit-btn');
+
 // setButtonLoginSubmit(): configura o botão do form de login
 function setButtonLoginSubmit() {
   // obtém o seletor do botão e acrescenta o event listener
@@ -11,6 +14,16 @@ function setButtonLoginSubmit() {
     }
   });
 }
+
+// Marcello, dá uma olhada aqui, não sei o que estou errando. Valeu!
+// function agreementCheck() {
+//   if (checkBox.checked) {
+//     buttonEnviar.disabled = false;
+//   } else {
+//     buttonEnviar.disabled = true;
+//   }
+// }
+// checkBox.addEventListener('click', agreementCheck);
 
 // executa as funções ao carregar a página
 window.onload = () => {
