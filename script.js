b46d5254b60bc6c3ff8b21e5b18f4f91f1b8b84b
Pablo Assunção <pabloassuncao2@gmail.com>
2021-07-19 15:06:51 -0300

    Little Boy e Fat Man

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const logIn = document.querySelector('#log-button');
 const email = document.querySelector('[name = log-in-email]');
 const password = document.querySelector('[name = log-in-password');
+const agreement = document.querySelector('#agreement');
+const submit = document.querySelector('#submit-btn');
 
 // Funções
 const allF = {
@@ -12,9 +14,18 @@ const allF = {
       alert('Login ou senha inválidos.');
     }
   },
+  checkSubmit: function submiter(event) {
+    if (event.target.checked === true) {
+      submit.disabled = false;
+    }
+    if (event.target.checked === false) {
+      submit.disabled = true;
+    }
+  },
 };
 
 // Desabilita o default
 // Event Listeners
 
 logIn.addEventListener('click', allF.checkLog);
+agreement.addEventListener('click', allF.checkSubmit);
