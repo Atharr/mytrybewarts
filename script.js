b9262695a78941666bd05730992b709ad809c8a4
Guinxard <lucasmfarias3@gmail.com>
2021-07-19 21:34:58 -0300

    update requisito 3

---
+++ script.js
@@ -0,0 +1,11 @@
+let loginBtn = document.getElementById('login-btn');
+let loginInput = document.getElementById('login-input');
+let passInput = document.getElementById('pass-input');
+
+loginBtn.addEventListener('click', () => {
+    if (loginInput.value != "tryber@teste.com" && passInput.value != "123456") {
+      alert ('Login ou senha inválidos.')
+    } else {
+      alert ('Olá, Tryber!'); 
+    }
+  });
