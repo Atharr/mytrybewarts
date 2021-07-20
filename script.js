cec5870128746a894ce295f33c39b9243547a9d1
Dogl4 <doougllas@hotmail.com.br>
2021-07-20 16:05:15 -0300

    c9 - Adiciona um select ao forms do body e um pouco de estilo no css em relaçao ao header.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let elemento = document.getElementById('button').addEventListener('click', (event) => {
+document.getElementById('button').addEventListener('click', (event) => {
   const t = document.getElementById('login').value === 'tryber@teste.com';
   const s = document.getElementById('senha').value === '123456';
-  event.preventDefault()
+  event.preventDefault();
   if (t && s) {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
   }
-})
+});
