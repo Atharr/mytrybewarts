ececada00d39c26d924dc35da1796b6c36f824d1
MartinSilva <martinempreendedor@gmail.com>
2021-07-20 15:22:58 -0300

    senha

---
+++ script.js
@@ -0,0 +1,10 @@
+let elemento = document.getElementById('button').addEventListener('click', (event) => {
+  const t = document.getElementById('login').value === 'tryber@teste.com';
+  const s = document.getElementById('senha').value === '123456';
+  event.preventDefault()
+  if (t && s) {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
+  }
+})
